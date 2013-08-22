22d8237d426f8a5e34edff8815f117a41b35ef15
bobo0684 <kh_wzb@hotmail.com>
2013-08-22 16:20:48 +0800

    第一次提交

---
+++ Contracts/ICalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+namespace Contracts
+{
+    [ServiceContract(Name = "CalculatorService")]
+  public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double x,double y);
+        [OperationContract]
+        double Substract(double x,double y);
+        [OperationContract]
+        double Multiply(double x,double y);
+        [OperationContract]
+        double Divide(double x,double y);
+
+    }
+}
