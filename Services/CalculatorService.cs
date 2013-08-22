c12b17c21a2bc92e24103718ed1d5a769f0f25c4
bobo0684 <kh_wzb@hotmail.com>
2013-08-22 16:33:13 +0800

    第二次提交

---
+++ Services/CalculatorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contracts;
+namespace Services
+{
+    public class CalculatorService:ICalculator
+    {
+
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            if (x > y)
+            {
+                return x - y;
+            }
+            else {
+
+                return y - x;
+            }
+         
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
